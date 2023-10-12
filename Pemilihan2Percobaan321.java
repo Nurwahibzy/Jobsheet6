041ccc1313447481224d6882d277133e972504f2
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-12 15:24:27 +0700

    Memodifikasi equalsIgnoreCase menjadi equals saja

--- Pemilihan2Percobaan321.java
+++ Pemilihan2Percobaan321.java
@@ -12,24 +12,24 @@ public class Pemilihan2Percobaan321 {
         kategori = input21.nextLine();
         System.out.print("Masukkan Besarnya Penghasilan : ");
         penghasilan = input21.nextInt();
-        if (kategori.equalsIgnoreCase("pekerja")) {
-            if (penghasilan <= 2000000) {
+        if (kategori.equals("pekerja")) {
+            if (penghasilan <= 2000000) 
                 pajak = 0.1;
-            } else if (penghasilan <= 3000000) {
+             else if (penghasilan <= 3000000) 
                 pajak = 0.15;
-            } else {
+            else 
                 pajak = 0.2;
-            }
+            
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.print("Penghasilan Bersih : " + gajiBersih);
-        } else if (kategori.equalsIgnoreCase("pebisnis")) {
-            if (penghasilan <= 2500000) {
+        } else if (kategori.equals("pebisnis")) {
+            if (penghasilan <= 2500000) 
                 pajak = 0.15;
-            } else if (penghasilan <= 3500000) {
+             else if (penghasilan <= 3500000) 
                 pajak = 0.2;
-            } else {
+             else 
                 pajak = 0.25;
-            }
+            
             gajiBersih = (int) (penghasilan - (pajak * penghasilan));
             System.out.print("Penghasilan Bersih : " + gajiBersih);
 
