7304c5eec33646a28b3226b49dacaccc74d89b22
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-12 15:10:42 +0700

    Menambahkan pemilihan untuk segitiga sama sisi, kaki , sembarang

--- Pemilihan2Percobaan221.java
+++ Pemilihan2Percobaan221.java
@@ -15,12 +15,16 @@ public class Pemilihan2Percobaan221 {
         sudut3 = input21.nextInt();
         totalSudut = sudut1 + sudut2 + sudut3;
         if (totalSudut == 180) {
-            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-            } else {
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-            }
-        }else {
+
+            else if ((sudut1 == sudut2) && (sudut1 == sudut3) && (sudut2 == sudut3))
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            else if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3))
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            else 
+                System.out.println("Segitiga tersebut adalah segitiga sembarang");
+        } else {
             System.out.println("Bukan Segitiga");
         }
 
